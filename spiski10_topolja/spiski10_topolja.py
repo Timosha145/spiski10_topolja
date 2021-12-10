d2949a6866257585f7ab73ebb556a88bea0c7d87
Timosha145 <93324319+Timosha145@users.noreply.github.com>
2021-12-10 09:37:32 +0200

    Update spiski10_topolja.py

--- spiski10_topolja/spiski10_topolja.py
+++ spiski10_topolja/spiski10_topolja.py
@@ -9,12 +9,12 @@ while 1:
     print('дублировать текст (2)')
     print('добавить внутрь текста элемент (3)')
     print('удалить один определённый элемент (4)')
-    print('поменять один элемент с другим элементом местами (5)')
-    print('удалить один определённый элемент (6)')
-    print('удалить один определённый элемент (7)')
-    print('сортировать текст (8)')
+    print('переместить один определённый элемент (5)')
+    print('сделать все буквы большими или маленькими (6)')
+    print('узнать количество символов в тексте (7)')
+    print('сортировать текст в правильном или обратном направлении (8)')
     print('перевернуть текст (9)')
-    print('удалить текст (10)')
+    print('запятая после каждой буквы (10)')
     while type(answ)!=int or answ>10 or answ<=0:
         try:
             answ=int(input('Введите вариант ответа 1-10: '))
@@ -23,18 +23,51 @@ while 1:
     if answ==1:
         letter=input('Какой элемент добавить в конец текста?: ')
         list_1.append(letter)
-        print('Текст изменён на -',list_1)
+        print('Текст изменён на -',''.join(list_1))
     if answ==2:
         list_1.extend(list_1)
-        print('Текст изменён на -',list_1)
+        print('Текст изменён на -',''.join(list_1))
     if answ==3:
         letter=input('Какой элемент добавить в конец текста?: ')
         place=int(input('После какого по счёту элемента вставить выбранный элемент?: '))
+        place=place+1
         list_1.insert(place, letter)
-        print('Текст изменён на -',list_1)
+        print('Текст изменён на -',''.join(list_1))
+    if answ==4:
+        letter=input('Какой элемент хотите удалить?: ')
+        list_1.remove(letter)
+        print('Текст изменён на -',''.join(list_1))
+    if answ==5:
+        letter=input('Какой элемент хотите выбрать?: ')
+        place=int(input('После какого по счёту элемента вставить выбранный элемент?: '))
+        place=place+1
+        list_1.remove(letter)
+        print('Текст изменён на -',''.join(list_1))
+    if answ==6:
+        BorS=int(input('Сделать большими(1) или маленькими(0)'))
+        if BorS==1:
+            print('Текст изменён на -',''.join(list_1).upper())
+        if BorS==0:
+            print('Текст изменён на -',''.join(list_1).lower())
+        list_1 = list(tekst.strip(" "))
+    if answ==7:
+        nSimvolov=len(list_1)
+        print(f'В тексте - {nSimvolov} символа')
+    if answ==8:
+        PRAVElorOBRAT=int(input('В правильном направлении(1) или в обратном направлении(0)'))
+        if PRAVElorOBRAT==1:
+            list_1.sort()
+        if PRAVElorOBRAT==0:
+            list_1.sort(reverse=True)
+        print('Текст изменён на -',''.join(list_1))
+    if answ==9:
+        list_1.reverse()
+        print('Текст изменён на -',''.join(list_1))
+    if answ==10:
+        print('Текст изменён на -',', '.join(list_1))
     answ=0
     cont=int(input('Хотите продолжить редактирование? Да(1), Нет(0): '))
     print()
     if cont==0:
         break
-print('good')
+print('Спасибо за использования программы "SuperTextEditor"')
